a702dcb863669db977c770ee794e2f7869be4239
nithinvarma <nithinvarma411@gmail.com>
2025-04-19 09:39:36 +0530

    added rate-limiter

--- server/src/routes/invoice.routes.js
+++ server/src/routes/invoice.routes.js
@@ -1,13 +1,74 @@
 import { createInvoice, getInvoices, updateInvoice, deleteInvoice, getLatestInvoice, sendInvoiceEmail } from "../controllers/invoice.controller.js";
 import { Router } from 'express';
+import rateLimit from 'express-rate-limit';
 
 const router = Router();
 
-router.route("/createInvoice").post(createInvoice);
-router.route("/getInvoices").get(getInvoices);
-router.route("/updateInvoice/:id").put(updateInvoice);
-router.route("/deleteInvoice/:id").delete(deleteInvoice);
-router.route("/latest-invoice").get(getLatestInvoice);
-router.route("/send-email").post(sendInvoiceEmail);
+const createInvoiceLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 10,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many Invoices generated. Try again later.'
+        });
+      }
+});
+
+const getInvoicesLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 200,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many requests. Try again later.'
+        });
+      }
+});
+
+const updateInvoicesLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many requests. Try again later.'
+        });
+      }
+});
+
+const deleteInvoicesLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many requests. Try again later.'
+        });
+      }
+});
+
+const latestInvoicesLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 200,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many requests. Try again later.'
+        });
+      }
+});
+
+const sendEmailLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many requests. Try again later.'
+        });
+      }
+});
+
+router.route("/createInvoice").post(createInvoiceLimiter, createInvoice);
+router.route("/getInvoices").get(getInvoicesLimiter, getInvoices);
+router.route("/updateInvoice/:id").put(updateInvoicesLimiter,updateInvoice);
+router.route("/deleteInvoice/:id").delete(deleteInvoicesLimiter, deleteInvoice);
+router.route("/latest-invoice").get(latestInvoicesLimiter, getLatestInvoice);
+router.route("/send-email").post(sendEmailLimiter, sendInvoiceEmail);
 
 export default router;
--- server/src/routes/stock.routes.js
+++ server/src/routes/stock.routes.js
@@ -1,10 +1,31 @@
 import { addStock, getStock, updateStock, deleteStock, getStockByGroup, createGroup, deleteGroup, removeFromGroup, stockSuggestions } from "../controllers/stock.controller.js";
 import {Router} from 'express';
+import rateLimit from 'express-rate-limit';
 
 const router = Router();
 
-router.route("/addStock").post(addStock);
-router.route("/getStock").get(getStock);
+const addStockLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many requests. Try again later.'
+        });
+      }
+});
+
+const getStockLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100,
+    handler: (req, res) => {
+        res.status(429).json({
+          message: 'Too many requests. Try again later.'
+        });
+      }
+});
+
+router.route("/addStock").post(addStockLimiter, addStock);
+router.route("/getStock").get(getStockLimiter, getStock);
 router.route("/getStockByGroup/:group").get(getStockByGroup);
 router.route("/createGroup").post(createGroup);
 router.route("/update/:id").put(updateStock);

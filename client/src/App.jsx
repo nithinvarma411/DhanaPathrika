c9d0a4cf609e47e8eac4649ffbea293cf805b16a
nithinvarma <nithinvarma411@gmail.com>
2025-05-14 18:08:36 +0530

    solved minor bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,102 +1,43 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import CompanyForm from './pages/CompanyForm';
-import Dashboard from './pages/Dashboard';
-import InvoicePage from './pages/InvoicePage';
-import BillingPage from './pages/BillingPage';
-import InvoiceGenerator from './pages/InvoiceGenerator';
-import StockMaintainance from './pages/StockMaintainance';
-import AddStock from './pages/AddStock';
-import NotFound from './pages/NotFound';
-import JoinSandbox from './pages/JoinSandbox';
-import SendMessage from './pages/SendMessage';
-import Notification from './pages/Notification';
-import PasswordRecovery from './pages/PasswordRecovery';
-
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import CompanyForm from "./pages/CompanyForm";
+import Dashboard from "./pages/Dashboard";
+import InvoicePage from "./pages/InvoicePage";
+import BillingPage from "./pages/BillingPage";
+import InvoiceGenerator from "./pages/InvoiceGenerator";
+import StockMaintainance from "./pages/StockMaintainance";
+import AddStock from "./pages/AddStock";
+import NotFound from "./pages/NotFound";
+import JoinSandbox from "./pages/JoinSandbox";
+import SendMessage from "./pages/SendMessage";
+import Notification from "./pages/Notification";
+import PasswordRecovery from "./pages/PasswordRecovery";
 
 function App() {
-
   return (
     <BrowserRouter basename="/">
       <Routes>
-        <Route path='/' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/details' element={<CompanyForm />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/details" element={<CompanyForm />} />
 
         {/* Protected Routes */}
-        <Route
-          path='/home'
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/profile'
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/Bills'
-          element={
-            <ProtectedRoute>
-              <BillingPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/invoice-generator'
-          element={
-            <ProtectedRoute>
-              <InvoiceGenerator />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/invoice'
-          element={
-            <ProtectedRoute>
-              <InvoicePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/stock-maintenance'
-          element={
-            <ProtectedRoute>
-              <StockMaintainance />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/notification'
-          element={
-            <ProtectedRoute>
-              <Notification />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path='/add-stock'
-          element={
-            <ProtectedRoute>
-              <AddStock />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Dashboard />} />
+        <Route path="/Bills" element={<BillingPage />} />
+        <Route path="/invoice-generator" element={<InvoiceGenerator />} />
+        <Route path="/invoice" element={<InvoicePage />} />
+        <Route path="/stock-maintenance" element={<StockMaintainance />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/add-stock" element={<AddStock />} />
 
-        <Route path='/password-recovery' element={<PasswordRecovery />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/password-recovery" element={<PasswordRecovery />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <ToastContainer position="top-right" autoClose={1000} />
     </BrowserRouter>
